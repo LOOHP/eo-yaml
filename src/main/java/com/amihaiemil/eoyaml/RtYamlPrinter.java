58b8b426ec009b9e13ac4c1eba7d6615dd204967
LOOHP <jamesloohp@gmail.com>
2022-01-29 02:48:15 +0000

    Escape \\ in quoted strings

--- src/main/java/com/amihaiemil/eoyaml/RtYamlPrinter.java
+++ src/main/java/com/amihaiemil/eoyaml/RtYamlPrinter.java
@@ -426,7 +426,7 @@ final class RtYamlPrinter implements YamlPrinter {
                 if(value.contains("\"")) {
                     escaped = "'" + value + "'";
                 } else {
-                    escaped = "\"" + value + "\"";
+                    escaped = "\"" + value.replace("\\", "\\\\") + "\"";
                 }
             }
             return escaped;
